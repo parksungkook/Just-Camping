399d0d2806f624c8689f35b510754018a5a6a10c
linjee <67717819+linjee@users.noreply.github.com>
2020-10-16 10:05:15 +0900

    2020.10.16

---
+++ Assets/SMJ_/Scripts/LevelChanger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelChanger : MonoBehaviour
+{
+    public Animator animator;
+
+    private void Start()
+    {
+
+        Invoke("FadeToLevel", 1);
+    }
+    public void FadeToLevel(int levelIndex)
+    {
+        animator.SetTrigger("Fade_Out");
+    }
+}
